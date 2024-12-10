0948fcf3fcc0b7d8f55ab5fda6ef2158935b0c97
GouravRajak45 <rajakgourav925@gmail.com>
2024-12-10 23:39:19 +0530

    Creating Response Structure

---
+++ src/main/java/com/example/vra/responsestructure/ResponseStructure.java
@@ -0,0 +1,35 @@
+package com.example.vra.responsestructure;
+
+public class ResponseStructure<T> {
+
+	private int status;
+	private String massage;
+	private T data;
+	
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMassage() {
+		return massage;
+	}
+	public void setMassage(String massage) {
+		this.massage = massage;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	public static <T>ResponseStructure<T> create(int status,String massage,T data){
+		ResponseStructure<T> response = new ResponseStructure<T>();
+		response.setStatus(status);
+		response.setMassage(massage);
+		response.setData(data);
+		return response;
+	}
+}
