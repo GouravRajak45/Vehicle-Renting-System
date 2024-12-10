afff7d22a977387da19743bbe8dc25e649ec81a3
GouravRajak45 <rajakgourav925@gmail.com>
2024-12-10 15:42:17 +0530

    Project Created

---
+++ src/main/java/com/example/vra/VehicleRantingApplication.java
@@ -0,0 +1,13 @@
+package com.example.vra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRantingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRantingApplication.class, args);
+	}
+
+}
