3b25ab94349671d1218000fec068d9721a7ce933
GouravRajak45 <rajakgourav925@gmail.com>
2024-12-16 00:46:26 +0530

    Inserting Vehicle data

--- src/main/java/com/example/vra/controller/VehicleController.java
+++ src/main/java/com/example/vra/controller/VehicleController.java
@@ -1,7 +1,13 @@
 package com.example.vra.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.vra.entity.Vehicle;
+import com.example.vra.responsestructure.ResponseStructure;
 import com.example.vra.service.VehicleService;
 
 @RestController
@@ -14,5 +20,10 @@ public class VehicleController {
 		this.vehicleService = vehicleService;
 	}
 	
-	
+	@PostMapping("/save-vehicle")
+	public ResponseEntity<ResponseStructure<Vehicle>> saveVehicle(@RequestBody Vehicle vehicle) {
+		Vehicle vehicle2= vehicleService.addVehicle(vehicle);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(ResponseStructure.create(HttpStatus.CREATED.value(), "Vehicle Data inserted", vehicle2));
+	}
 }
--- src/main/java/com/example/vra/service/VehicleService.java
+++ src/main/java/com/example/vra/service/VehicleService.java
@@ -2,6 +2,7 @@ package com.example.vra.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.vra.entity.Vehicle;
 import com.example.vra.repository.VehicleRepository;
 
 @Service
@@ -12,6 +13,11 @@ public class VehicleService {
 	public VehicleService(VehicleRepository vehicleRepository) {
 		super();
 		this.vehicleRepository = vehicleRepository;
+	}
+
+	public Vehicle addVehicle(Vehicle vehicle) {
+		Vehicle vehicle2= vehicleRepository.save(vehicle);
+		return vehicle2;
 	}
 	
 	
